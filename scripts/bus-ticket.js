9e02aef8c2fb845d2d80b5073855de81a29557d6
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-02-19 09:01:52 +0600

    added the dynamic to selected box

--- scripts/bus-ticket.js
+++ scripts/bus-ticket.js
@@ -10,16 +10,72 @@
 // Scroll to get Ticket
 function getTicketById(getTicket) {
     const pParibahan = document.getElementById(getTicket);
-    if(pParibahan) {
-        pParibahan.scrollIntoView({behavior: 'smooth'});
+    if (pParibahan) {
+        pParibahan.scrollIntoView({ behavior: 'smooth' });
     };
 };
 
-
+// Change The backgroun of btn & get specific btn
 const ticketBtns = document.getElementsByClassName('ticket-btn');
-for(const btn of ticketBtns) {
-    btn.addEventListener('click', function(e) {
-        console.log('clicked');
+for (const btn of ticketBtns) {
+
+    btn.addEventListener('click', function (e) {
+        btn.classList.add('bg-green-400', 'text-white');
+
+        const specificBtn = e.target.id;
+        // console.log(specificBtn);
+
+        // return specificBtn;
+
+
+        // Create elements to show in the box
+        const showBtnSeat = document.createElement('li');
+        showBtnSeat.innerText = specificBtn;
+
+        const showClass = document.createElement('li');
+        showClass.innerText = 'Economy';
+        
+        const showPrice = document.createElement('li');
+        showPrice.innerText = '550';
+
+        // Create li container
+        const ul = document.createElement('ul');
+        ul.appendChild(showBtnSeat);
+        ul.appendChild(showClass);
+        ul.appendChild(showPrice);
+        
+        ul.classList.add('flex');
+        ul.classList.add('justify-around');
+        
+        // Append the ul in the Show Box
+        const showSelectedSeats = document.getElementById('show-selected-seat');
+        showSelectedSeats.appendChild(ul);
+
+
     });
-    console.log()
 };
+
+
+
+// 
+
+
+
+// console.log(showSelectedSeats)
+
+
+
+// function getTicketButton() {
+//     const ticketBtns = document.getElementsByClassName('ticket-btn');
+//     console.log(ticketBtns)
+
+//     for (const btn of ticketBtns) {
+//         console.log(btn);
+//     }
+// };
+
+
+// function setButtonColor(e) {
+//     const btnColor = getElementById(e);
+//     btnColor.classList.add('bg-green-400', 'text-white');
+// };
